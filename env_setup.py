0d4fdf91a10c5b6e5c1c466e4721d4937cfee872
Himadri_profile <148573761+himadri991@users.noreply.github.com>
2025-07-05 16:28:14 +0530

    Update env_setup.py

--- env_setup.py
+++ env_setup.py
@@ -2,11 +2,11 @@ import os
 
 # API Keys
 env_vars = {
-    "GROQ_API_KEY": "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",
-    "TAVILY_API_KEY": "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",
-    "GOOGLE_API_KEY": "AIzaSyDwofVzJMk150Xz2FGq0DiLwfs_bsOcWYk",
-    "GOOGLE_CSE_ID": "b435d735c20ac472b",
-    "PINECONE_API_KEY": "pcsk_6Z5pw9_Na1i9SYu7WLthh8q6xo8B2JPSA7K4MseBC99RyqRkXuyZUsVCYHcCn7kf4FUpqm",
+    "GROQ_API_KEY": "",
+    "TAVILY_API_KEY": "",
+    "GOOGLE_API_KEY": "",
+    "GOOGLE_CSE_ID": "",
+    "PINECONE_API_KEY": "",
     "PINECONE_ENVIRONMENT": "us-east-1"
 }
 
